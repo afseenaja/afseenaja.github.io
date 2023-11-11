be1ba6419c8305056c10eacaa6d90ca8e9f3796c
afseenaja <afseenaja@gmail.com>
2023-11-11 17:13:04 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+// var swiper = new Swiper(".slide-content", {
+//     slidesPerView: 3,
+//     spaceBetween: 25,
+//     loop: true,
+//     centerSlide: 'true',
+//     fade: 'true',
+//     grabCursor: 'true',
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       dynamicBullets: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+
+//     breakpoints:{
+//         0: {
+//             slidesPerView: 1,
+//         },
+//         520: {
+//             slidesPerView: 2,
+//         },
+//         950: {
+//             slidesPerView: 3,
+//         },
+//     },
+//   });
